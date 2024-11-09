7e8424b9488f3d44a3d6ec140d7d1dd309c0bb60
rbech <rbech@newmind.ai>
2024-11-09 03:18:46 +0200

    Update helpers.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 
 import yaml
 import json
+import base64
 
 def read_config(config_path =  'config.yaml'):
     with open(config_path, 'r') as file:
@@ -15,3 +16,20 @@ def save_to_json(data, file_path):
 def read_json(file_path):
     with open(file_path, "r", encoding="utf-8")as fp:
         return json.load(fp)
+    
+
+def singleton(cls):
+    instances = {}
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    
+    return get_instance
+
+def encode_image(image_file):
+    return base64.b64encode(image_file.getvalue()).decode('utf-8')
+
+def generator_simulator(content: str):
+    for token in content.split(" "):
+        yield token + " "
